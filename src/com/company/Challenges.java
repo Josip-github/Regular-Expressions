81b36adc373bad17b5b697d2954cf803457075e0
Josip-github <josipcota1@gmail.com>
2022-02-19 19:11:48 +0100

    Challenge 1

---
+++ src/com/company/Challenges.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Challenges {
+
+    public static void main(String[] args) {
+
+        // Challenge 1: write the string literal regular expression that will match "I want a bike"
+        // Use the String.matches() to verify your answer
+
+        String challenge1 = "I want a bike.";
+        System.out.println(challenge1.matches("I want a bike."));
+
+
+    }
+}
