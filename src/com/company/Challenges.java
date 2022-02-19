ad7838721e97054567b8bb8ef42e75816eec8872
Josip-github <josipcota1@gmail.com>
2022-02-19 20:06:34 +0100

    challenge 7

--- src/com/company/Challenges.java
+++ src/com/company/Challenges.java
@@ -55,6 +55,14 @@ public class Challenges {
         System.out.println(challenge5.matches("^a{3}bc{8}d{3}ef{3}g$"));
         System.out.println(challenge5.replaceAll("^a{3}bc{8}d{3}ef{3}g$", "REPLACED"));
 
+        // Challenge 7: Write a regular expression that will match a string that starts with a series of letters.
+        // The letters must be followed by a period. After the period, there must be a series of digits.
+        // The string "kjisl.22" would match. The string "f5.12a" would not. Use this string to test your reg expression
+
+        String challenge7 = "abcd.135";
+        System.out.println(challenge7.matches("^[A-z][a-z]+\\.\\d+$"));
+
+
 
 
     }
