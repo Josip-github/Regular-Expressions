e625872b553a76a7cc4cb422ec3d2de5efb6fb29
Josip-github <josipcota1@gmail.com>
2022-02-19 21:46:01 +0100

    challenge 12

--- src/com/company/Challenges.java
+++ src/com/company/Challenges.java
@@ -117,5 +117,11 @@ public class Challenges {
             System.out.println("Occurence: " + matcher11a.group(1));
         }
 
+        // Challenge 12: Write a regular expression that will match a 5-digit US zip code.
+        // Use "11111" as your test string
+
+        String challenge12 = "11111";
+        System.out.println(challenge12.matches("^\\d{5}$"));
+
     }
 }
